726551a0ae072ebf5240505a2ca36dbf1b217d17
shafik720 <shafikrasel5@gmail.com>
2024-04-02 18:29:40 +0600

    added filtering ui with shadCn

--- src/pages/todoContainer.tsx
+++ src/pages/todoContainer.tsx
@@ -5,9 +5,23 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import TodoCard from "./todoCard";
 import { useAppSelector } from "@/Redux/hook";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
 
 const TodoContainer = () => {
-  const todos = useAppSelector(state => state.todo.todos);
+  const todos = useAppSelector((state) => state.todo.todos);
+
+  // --- dropdown menu for filter
+  const [filter, setFilter] = useState("mid")
+
   return (
     <div>
       <div className="my-5 ">
@@ -19,12 +33,34 @@ const TodoContainer = () => {
           </DialogTrigger>
           <AddTodoModal />
         </Dialog>
+
+        {/* --- Dropdown Menu For Filter --- */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Filter</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel className="text-blue-600">Select Priority</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuRadioGroup
+              value={filter}
+              onValueChange={setFilter}
+              defaultValue={'mid'}
+            >
+              <DropdownMenuRadioItem value="high">High</DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="mid">
+                Mid
+              </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="low">Low</DropdownMenuRadioItem>
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <button className="border-2 px-4 me-3 rounded-sm py-2">Filter</button>
       </div>
       <div>
-        {
-          todos.map(todo=><TodoCard {...todo} />)
-        }
+        {todos.map((todo) => (
+          <TodoCard {...todo} />
+        ))}
       </div>
     </div>
   );
